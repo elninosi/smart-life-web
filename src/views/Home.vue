dbd3faa0ad67080467162050ce678f449438c20b
elninosi <nino.vranesic@elnino.si>
2022-05-11 21:14:14 +0200

    test api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,7 +53,7 @@
           </el-avatar>
         </el-tooltip>
         <span class="device-name">{{ device.name }}</span>
-        <span class="device-desc">{{ device.desc }}</span>
+        <span class="device-id">{{ device.id }}</span>
 
         <template v-if="device.type === 'scene'">
           <el-button type="default" circle class="icon-big trigger"
