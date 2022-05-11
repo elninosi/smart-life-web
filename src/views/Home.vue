d33f4a3e1d6c084cd67e90c49b22e6482a832406
elninosi <nino.vranesic@elnino.si>
2022-05-11 23:35:54 +0200

    device type test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,7 +53,7 @@
           </el-avatar>
         </el-tooltip>
         <p class="device-name">{{ device.name }}</p>
-        <p class="device-status">{{ device.dev_type }}</p>
+        <p class="device-status">{{ device.type }}</p>
 
         <template v-if="device.type === 'scene'">
           <el-button type="default" circle class="icon-big trigger"
